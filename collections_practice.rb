3797373a527cdd41022d66131ee0901861cf32b1
Knika Dass <knikadass@gmail.com>
2020-07-16 20:57:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,12 @@ end
 def sort_array_desc (array)
   array.sort {|x,y| y <=> x }
 end
+
+def sort_array_char_count
+  array.sort
+end
+
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
