aa5c0a1d97d1251db2cc5b646e1b6b15d0059e13
Knika Dass <knikadass@gmail.com>
2020-07-16 21:02:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -20,8 +20,8 @@ def reverse_array (array)
 end
 
 def kesha_maker (array)
-  array.collect do |element|
-    element[2] = "$"
-    element
+  array.collect do |sign|
+    sign[2] = "$"
+    sign
   end
 end
