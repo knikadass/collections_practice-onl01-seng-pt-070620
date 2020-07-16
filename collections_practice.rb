c5e6dec6714eb6099851a0c8df7459db8d737d6d
Knika Dass <knikadass@gmail.com>
2020-07-16 21:03:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,3 +32,20 @@ def find_a (array)
   end
 end
 
+def sum_array (array)
+  array.inject do |sum, num|
+    sum + num
+  end
+end
+
+
+
+def add_s (array)
+  array.collect.with_index do |word, index|
+    if index == 1 
+      word 
+    else 
+      word + "s"
+    end
+  end
+end
